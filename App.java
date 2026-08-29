dfa75c49db31921361fc68f35738d5898b8ec314
Rian Oliveira Dos Santos <rianolivera1738.com@gmail.com>
2026-08-29 15:30:40 +0000

    variavel9

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+public static void main(String[] args) throws Exception {
+double celsius = 25.0;
+double fahrenheit = (celsius * 1.8) + 32;
+System.out.println("Temperatura em Celsius: " + celsius + " °C");
+System.out.println("Temperatura em Fahrenheit: " + fahrenheit + " °F");
+}}
+
